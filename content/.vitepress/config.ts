a44ba39a1a79f996aad7c588832dea8a1e538635
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2023-04-21 14:52:23 +0800

    [M] Move contents to ./content

--- content/.vitepress/config.ts
+++ content/.vitepress/config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
       ],
     },
 
-    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }],
+    socialLinks: [{ icon: 'github', link: 'https://github.com/one-among-us' }],
   },
   rewrites: { 'posts/index.md': 'posts.md' },
   vite: { plugins: [VueJsx(), Unocss()] },
